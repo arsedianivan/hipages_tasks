316e9b2235141167b08ef91c932f0600bc102728
arsedianivan <arsedian.ivan@gmail.com>
2024-07-16 00:47:12 +1000

    Add comments to explain each steps of the SQL

--- Task3.sql
+++ Task3.sql
@@ -1,4 +1,5 @@
 WITH CTE_1 AS (
+	--The names and the number of messages sent by each user
 	SELECT
 		Task = '1'
 		,Col1 = COUNT(msg.MessageID)
@@ -8,6 +9,7 @@ WITH CTE_1 AS (
 			ON msg.UserIDSender = sender.UserID
 	Group By sender.UserID
 ),  CTE_2 AS (
+	--The total number of messages sent stratified by weekday
 	SELECT
 		Task = '2'
 		,Col1 = COUNT(msg.MessageID)
@@ -16,6 +18,7 @@ WITH CTE_1 AS (
 	WHERE DATE_PART(dayofweek, DateSent) BETWEEN 1 AND 5
 	GROUP BY DATE_PART(dayofweek, DateSent)
 ), CTE_3 AS (
+	--The most recent message from each thread that has no response yet
 	SELECT DISTINCT
 		Task = '3'
 		,Col1 = Msg.MessageContent
@@ -25,6 +28,7 @@ WITH CTE_1 AS (
 			ON thr.ThreadID = msg.ThreadID
 	WHERE COUNT(MessageID) = 1
 ), CTE_4 AS (
+	--For the conversation with the most messages: all user data and message contents ordered chronologically so one can follow the whole conversation.
 		SELECT
 			Task = '4'
 			,Col1 = ThreadID
@@ -44,7 +48,9 @@ WITH CTE_1 AS (
 							)
 		ORDER BY DateSent DESC
 		)
-		
+	/** 
+	Union all 4 CTEs to combine the result sets into a single query
+	**/
 		
 	SELECT Task, Col1, Col2, Col3='', Col4='', Col5='' FROM CTE_1 
 	UNION 
